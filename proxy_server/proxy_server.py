82a19613a9835174dde9520f75b809446dc5b4d0
Pratik Jain <pratik.jain448@gmail.com>
2018-02-13 13:21:41 +0530

    Proxy with threading

--- proxy_server/proxy_server.py
+++ proxy_server/proxy_server.py
@@ -11,6 +11,7 @@ class ProxyServer:
 
 	def close_socket(self):
 		self.server_socket.close()
+		os.exit(0)
 		return
 
 	def __init__(self, config):
@@ -39,88 +40,95 @@ class ProxyServer:
 
 		return method, url, http_ver
 
+	def request_handler(self, client_socket, addr):
+		print('\n================REQUEST INITIATED==================\n')
+		print('Receiving request from Client')
+		client_req = client_socket.recv(config.MAX_REQUEST_LEN).decode()
+
+		print('Separating client request line by line')
+		parsed_req = client_req.split(config.JOIN_DELIM)
+
+		# Parsing the First line of HTTP request header
+		print('Parsing')
+		print(parsed_req[0])
+		req_method, req_url, req_http_ver = self.__parse_request_header(
+															parsed_req[0]
+																);
+
+		# Printing parsed information
+		print(req_method)
+		print(req_url)
+		print(req_http_ver)
+
+		# Calculating the start of host name
+		host_start_pos = req_url.find(config.HTTP_REQUEST) +\
+							len(config.HTTP_REQUEST)
+		# Calculating the end of host name
+		host_end_pos = req_url.find(config.HOST_DELIMITER, host_start_pos)
+		# Calculating start of Port number
+		port_start_pos = host_end_pos + 1
+		# Calculating start of Host number
+		port_end_pos = req_url.find(config.PORT_DELIMITER, port_start_pos)
+
+		# Printing Host Name
+		print('HOST NAME:')
+		req_host_name = req_url[host_start_pos: host_end_pos]
+		print(req_host_name)
+		# Printing Port Name
+		print('PORT NUMBER:')
+		req_port_num = req_url[port_start_pos: port_end_pos]
+		print(req_port_num)
+		# Printing File Name
+		print('FILE NAME:')
+		req_file_name = req_url[port_end_pos + 1 : ]
+		print(req_file_name)
+
+		try:
+			# Forming request
+			print('Forming Request to original host')
+			parsed_req[0] = '%s /%s %s' % (req_method, req_file_name, req_http_ver)
+			print(parsed_req[0])
+			host_req = config.JOIN_DELIM.join(parsed_req)
+
+			# Initialising connection to the Requested Host Socket
+			host_socket = socket.socket()
+			host_socket.connect((req_host_name, int(req_port_num)))
+
+			# Forwarding Request to Host
+			print('Requesting to the Host')
+			host_socket.send(host_req.encode())
+
+			# Receiving request from Host
+			host_resp = host_socket.recv(config.MAX_REQUEST_LEN)
+			# Forwarding request from Host to Client
+			client_socket.send(host_resp)
+
+			data = host_socket.recv(config.MAX_REQUEST_LEN)
+			while  data:
+				client_socket.send(data)
+				data = host_socket.recv(config.MAX_REQUEST_LEN)
+
+			print('Client request fulfilled..!!')
+		except Exception as err:
+			print('Unexpected Error Occurred on Connecting to Host:')
+			print(err)
+		# Closing the Host Sockets
+		host_socket.close()
+		print('\n=================REQUEST TERMINATED==================\n')
+		return
+
 	def start_accepting(self):
 
 		# Forever accept
 		while True:
 			(client_socket, addr) = self.server_socket.accept()
-
-			print('\n================REQUEST INITIATED==================\n')
-			print('Receiving request from Client')
-			client_req = client_socket.recv(config.MAX_REQUEST_LEN).decode()
-
-			print('Separating client request line by line')
-			parsed_req = client_req.split(config.JOIN_DELIM)
-
-			# Parsing the First line of HTTP request header
-			print('Parsing')
-			print(parsed_req[0])
-			req_method, req_url, req_http_ver = self.__parse_request_header(
-																parsed_req[0]
-																	);
-
-			# Printing parsed information
-			print(req_method)
-			print(req_url)
-			print(req_http_ver)
-
-			# Calculating the start of host name
-			host_start_pos = req_url.find(config.HTTP_REQUEST) +\
-			 					len(config.HTTP_REQUEST)
-			# Calculating the end of host name
-			host_end_pos = req_url.find(config.HOST_DELIMITER, host_start_pos)
-			# Calculating start of Port number
-			port_start_pos = host_end_pos + 1
-			# Calculating start of Host number
-			port_end_pos = req_url.find(config.PORT_DELIMITER, port_start_pos)
-
-			# Printing Host Name
-			print('HOST NAME:')
-			req_host_name = req_url[host_start_pos: host_end_pos]
-			print(req_host_name)
-			# Printing Port Name
-			print('PORT NUMBER:')
-			req_port_num = req_url[port_start_pos: port_end_pos]
-			print(req_port_num)
-			# Printing File Name
-			print('FILE NAME:')
-			req_file_name = req_url[port_end_pos + 1 : ]
-			print(req_file_name)
-
-			try:
-				# Forming request
-				print('Forming Request to original host')
-				parsed_req[0] = '%s /%s %s' % (req_method, req_file_name, req_http_ver)
-				print(parsed_req[0])
-				host_req = config.JOIN_DELIM.join(parsed_req)
-
-				# Initialising connection to the Requested Host Socket
-				host_socket = socket.socket()
-				host_socket.connect((req_host_name, int(req_port_num)))
-
-				# Forwarding Request to Host
-				print('requesting to the Host')
-				host_socket.send(host_req.encode())
-
-				# Receiving request from Host
-				host_resp = host_socket.recv(config.MAX_REQUEST_LEN)
-				# Forwarding request from Host to Client
-				client_socket.send(host_resp)
-
-				data = host_socket.recv(config.MAX_REQUEST_LEN)
-				while  data:
-					client_socket.send(data)
-					data = host_socket.recv(config.MAX_REQUEST_LEN)
-
-				print('Client request fulfilled..!!')
-			except Exception as err:
-				print('Unexpected Error Occurred on Connecting to Host')
-				print(err)
-			# Closing the Host Sockets
-			host_socket.close()
+			dmn = threading.Thread(name='Client',
+									target=self.request_handler,
+									args=(client_socket, addr))
+			dmn.run()
 			# Closing the Client Sockets
 			client_socket.close()
-			print('\n=================REQUEST TERMINATED==================\n')
+		self.close_socket()
 
 if '__main__' == __name__:
 	server = ProxyServer(config)
